edba991c8169bdc7692e8bd6aee86b30f292fde9
Mike Chu <michael.chu@optimizely.com>
2022-07-05 10:27:56 -0400

    Fix first test

--- test/optimizely_flutter_sdk_test.dart
+++ test/optimizely_flutter_sdk_test.dart
@@ -4,36 +4,40 @@ import 'package:optimizely_flutter_sdk/optimizely_flutter_sdk.dart';
 
 void main() {
   const String testSDKKey = "KZbunNn9bVfBWLpZPq2XC4";
-  // const MethodChannel channel = MethodChannel('optimizely_flutter_sdk');
+  const MethodChannel channel = MethodChannel('optimizely_flutter_sdk');
 
-  // setUpAll(() async {
-  //   TestWidgetsFlutterBinding.ensureInitialized();
+  setUpAll(() async {
+    TestWidgetsFlutterBinding.ensureInitialized();
 
-  //   channel.setMockMethodCallHandler((MethodCall methodCall) async {
-  //     log.add(methodCall);
-  //     switch (methodCall.method) {
-  //       case 'start':
-  //         isRecording = true;
-  //         return null;
-  //       case 'stop':
-  //         isRecording = false;
-  //         return {
-  //           'duration': duration,
-  //           'path': path,
-  //           'audioOutputFormat': extension,
-  //         };
-  //       case 'isRecording':
-  //         return isRecording;
-  //       case 'hasPermissions':
-  //         return true;
-  //       default:
-  //         return null;
-  //     }
-  //   });
-  // });
+    channel.setMockMethodCallHandler((MethodCall methodCall) async {
+      // log.add(methodCall);
+      switch (methodCall.method) {
+        case 'initialize':
+          return {
+            'initialized': true
+          };
+      //   case 'start':
+      //     isRecording = true;
+      //     return null;
+      //   case 'stop':
+      //     isRecording = false;
+      //     return {
+      //       'duration': duration,
+      //       'path': path,
+      //       'audioOutputFormat': extension,
+      //     };
+      //   case 'isRecording':
+      //     return isRecording;
+      //   case 'hasPermissions':
+      //     return true;
+      //   default:
+      //     return null;
+      }
+    });
+  });
 
   tearDown(() {
-    // channel.setMockMethodCallHandler(null);
+    channel.setMockMethodCallHandler(null);
   });
 
   group('Flutter SDK', () {
@@ -42,7 +46,7 @@ void main() {
       var sdk = new OptimizelyFlutterSdk(testSDKKey);
 
       // Act
-      var client = sdk.initializeClient();
+      var client = await sdk.initializeClient();
 
       // Assert
       expect(client, isNotNull);
