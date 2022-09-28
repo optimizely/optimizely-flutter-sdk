fafd1cbd3e4990e7caf13558c5f3a30c5396eea8
Yasir Ali <yali@folio3.com>
2022-09-28 10:14:40 +0500

    adding support for macos and windows.

--- example/windows/flutter/generated_plugin_registrant.cc
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,9 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <optimizely_flutter_sdk/optimizely_flutter_sdk_plugin_c_api.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  OptimizelyFlutterSdkPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("OptimizelyFlutterSdkPluginCApi"));
 }
--- example/windows/flutter/generated_plugins.cmake
+++ example/windows/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  optimizely_flutter_sdk
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
